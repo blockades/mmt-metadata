ef1d72a2cec7c2d15651df24f639c6ce31953aaa
ameba23 <ameba23@systemli.org>
2018-04-26 16:20:17 +0200

    trying to get encryption working

--- mmt-metadata.js
+++ mmt-metadata.js
@@ -1,14 +1,30 @@
 
+// todo:
+//   electron front end
+//   encrypt messages
+//   interact with wallet
+//   re-write rates tool into node
 
 var pull = require('pull-stream')
 var ssbClient = require('ssb-client')
 
+var count = 0
+
 // will hold current payments
 var payments = {}
 
-ssbClient(function (err, sbot) {
-  console.log('ssb ready.')
+var verbose = true
 
+ssbClient(function (err, sbot) {
+  if (verbose) console.log('ssb ready.')
+
+  // In order for messages to be encrypted we need to specify recipients
+  // there can be a maximum of 7, which means if we wanted more we need multiple 
+  // messages
+  //
+  // in order to use this script you need to set this to your own public key
+  // or make new one for test perposes
+  var recipients = ['@vEJe4hdnbHJl549200IytOeA3THbnP0oM+JQtS1u+8o=.ed25519']
 
   var payment = {
     
@@ -24,7 +40,7 @@ ssbClient(function (err, sbot) {
     comments:       []
   }
 
-  //addPayment(sbot, payment)
+  // addPayment(sbot, payment, recipients)
 
   var paymentComment = {
 
@@ -33,12 +49,20 @@ ssbClient(function (err, sbot) {
     comment: 'this payment was a mistake'
   }
 
-  //addPaymentComment(sbot, paymentComment)
+  // addPaymentComment(sbot, paymentComment, recipients)
   
-  pull(sbot.createLogStream({ live: true }), pull.drain(processMsg))
+  //pull(sbot.createLogStream({ live: true }), pull.drain(processMsg))
+
+  pull(
+    sbot.createLogStream(),
+    pull.collect(function (err, msgs) {
+      msgs.forEach( function (item,index) {
+        decryptAndProcess(sbot,item)
+      } )
+    })
+  )
   //pull(sbot.messagesByType({ type: 'addMmtPaymentTest', live: true }), pull.drain(processMsg))
   
-
 
   sbot.close()  
 })
@@ -46,42 +70,89 @@ ssbClient(function (err, sbot) {
 function testPublish() {
   //publish a test message
   sbot.publish({ type: 'testtype', text: 'hello, scuttleverse' }, function (err, msg) {
-    console.log(msg.key) 
-    console.log(msg.value.author)
-    console.log(msg.value.content)
-
+    if (verbose) { 
+      console.log(msg.key) 
+      console.log(msg.value.author)
+      console.log(msg.value.content)
+    }
   })
 }
- 
-function processMsg (msg) {
+
+
+function decryptAndProcess (sbot,msg){
+      sbot.private.unbox(msg, function (err, msg) { 
+        if (msg) {       
+          switch(msg.value.content.type) {
+          
+            case 'addMmtPaymentTest':
+              if (verbose) { 
+                console.log('Found a payment:')
+                console.log(msg.value.content)
+              }
+
+              // if we've never seen this transaction before, add it
+              if (!payments[msg.value.content.payment.key]) {
+                payments[msg.value.content.payment.key] = msg.value.content.payment
+              } else {
+                // what to do here?
+              }
+              break
+            
+            case 'modifyMmtPaymentTest':
+              if (verbose) {
+                console.log('Found a payment comment:')
+                console.log(msg.value.content)
+              }
+              payments[msg.value.content.paymentComment.key].comments.push( {
+                author: msg.value.author,
+                comment: msg.value.content.paymentComment.comment
+              } )
+          }  
+        } 
+      })
+}
+
+function processMsg (message) {
    // process a message from the drain 
    // is this the right way to handle the end of the message stream?
   try {
-    if (msg) 
-      switch(msg.value.content.type) {
-        
-        case 'addMmtPaymentTest':
-          console.log('Found a payment:')
-          console.log(msg.value.content)
+    count++
+    console.log(count)
+    if (message)
+      console.log(message)
+      // attempt to decrypt message 
+      sbot.private.unbox(message, function (err, msg) { 
+        if (msg) {       
+          switch(msg.value.content.type) {
+          
+            case 'addMmtPaymentTest':
+              if (verbose) { 
+                console.log('Found a payment:')
+                console.log(msg.value.content)
+              }
 
-          // if we've never seen this transaction before, add it
-          if (!payments[msg.value.content.payment.key]) {
-            payments[msg.value.content.payment.key] = msg.value.content.payment
-          } else {
-            // what to do here?
-          }
-          break
-        case 'modifyMmtPaymentTest':
-          console.log('Found a payment comment:')
-          console.log(msg.value.content)
-          payments[msg.value.content.paymentComment.key].comments.push( {
-            author: msg.value.author,
-            comment: msg.value.content.paymentComment.comment
-          } )
-           
-      }
-
+              // if we've never seen this transaction before, add it
+              if (!payments[msg.value.content.payment.key]) {
+                payments[msg.value.content.payment.key] = msg.value.content.payment
+              } else {
+                // what to do here?
+              }
+              break
+            
+            case 'modifyMmtPaymentTest':
+              if (verbose) {
+                console.log('Found a payment comment:')
+                console.log(msg.value.content)
+              }
+              payments[msg.value.content.paymentComment.key].comments.push( {
+                author: msg.value.author,
+                comment: msg.value.content.paymentComment.comment
+              } )
+          }  
+        } 
+      })
   } catch(e) {
+    // when we've processed all messagese
     displayPayments()
     return false
   }
@@ -102,27 +173,25 @@ function pullWithFeedStream() {
 
 
 
-function addPayment(sbot, paymentToAdd) {
+function addPayment(sbot, paymentToAdd, recipients) {
   
-  sbot.publish({ type: 'addMmtPaymentTest', payment: paymentToAdd }, function (err, msg) {
-    console.log('Adding payment:')
-    console.log(msg.key) 
-    console.log(msg.value.author)
-    console.log(msg.value.content)
-
+  sbot.private.publish({ type: 'addMmtPaymentTest', payment: paymentToAdd }, recipients, function (err, msg) {
+    if (verbose) {
+      console.log('Adding payment:')
+      console.log(JSON.stringify(msg, null, 4)) 
+    }
   })
 
 }
 
 
-function addPaymentComment(sbot, paymentComment) {
+function addPaymentComment(sbot, paymentComment, recipients) {
 
-  sbot.publish({ type: 'modifyMmtPaymentTest', paymentComment: paymentComment }, function (err, msg) {
-    console.log('Adding payment Comment:')
-    console.log(msg.key) 
-    console.log(msg.value.author)
-    console.log(msg.value.content)
-
+  sbot.private.publish({ type: 'modifyMmtPaymentTest', paymentComment: paymentComment }, recipients, function (err, msg) {
+    if (verbose) {
+      console.log('Adding payment Comment:')
+      console.log(JSON.stringify(msg, null, 4)) 
+    }
   })
 
 }
