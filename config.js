f669b00e8bc034039b4959c1fa7363d1874f09c6
Kieran <kieran.a.gibb@gmail.com>
2018-06-04 10:41:48 +1200

    setup parallel server and app electron processes

---
+++ config.js
@@ -0,0 +1,24 @@
+const Config = require('ssb-config/inject')
+const Keys = require('ssb-keys')
+const Path = require('path')
+
+module.exports = function () {
+  var config
+
+  return configure()
+
+  function configure () {
+    if (!config) {
+      const appName = process.env.APP_NAME || 'ssb' // this is your default identity
+      const port = process.env.PORT || 8808
+
+      const opts = { port }
+
+      config = Config(appName, opts)
+      config.keys = Keys.loadOrCreateSync(Path.join(config.path, 'secret'))
+      config.remote = `net:127.0.0.1:${config.port}~shs:${config.keys.id.slice(1).replace('.ed25519', '')}`
+    }
+    return config
+  }
+}
+
