24ec1be35a81c6064a14863f86894b905ad09dea
ameba23 <ameba23@systemli.org>
2018-06-07 10:32:50 +0200

    wrote my first test with tape

--- test/identify_wallet.js
+++ test/identify_wallet.js
@@ -1,26 +1,55 @@
+const test = require("tape");
 
-var wallets = {
-  'blah': {
-     foo: 5,
-     xpub: {
-       'bar': 6,
-       'noKey': 7
-     }
-   },
-   'theone': {
+// my first go at writing a test with tape
 
-     foo: 5,
-     xpub: {
-       'bar': 6,
-       'myKey': 7
-     }
-   }
+function identifyWallet(wallets, mpk) {
+  return Object.keys(wallets).find(function(wallet) {
+    return Object.keys(wallets[wallet].xpub).indexOf(mpk) > -1;
+  });
 }
 
-function indentifyWallet(mpk) {
-  return Object.keys(wallets).find ( function (wallet){
-    return Object.keys(wallets[wallet].xpub).indexOf(mpk) > -1
-  } )
-} 
+test("indentifyWallet should return the wallet key of the wallet containing the desired xpub", function(t) {
+  var wallets = {
+    blah: {
+      foo: 5,
+      xpub: {
+        bar: 6,
+        noKey: 7
+      }
+    },
+    theone: {
+      foo: 5,
+      xpub: {
+        bar: 6,
+        myKey: 7
+      }
+    }
+  };
+  
+  t.equal('theone', identifyWallet(wallets, 'myKey')); // make this test pass by completing the add function!
+  t.end();
+});
 
-console.log(indentifyWallet('myxKey'))
+
+test("indentifyWallet should return undefined if no wallet contains the desired xpub", function(t) {
+  var wallets = {
+    blah: {
+      foo: 5,
+      xpub: {
+        bar: 6,
+        noKey: 7
+      }
+    },
+    theone: {
+      foo: 5,
+      xpub: {
+        bar: 6,
+        myKey: 7
+      }
+    }
+  };
+  
+  t.false(identifyWallet(wallets, 'thisKeyDoesntExist')); // make this test pass by completing the add function!
+  t.end();
+});
+
